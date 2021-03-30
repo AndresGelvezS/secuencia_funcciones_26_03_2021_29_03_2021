2c2c45bffad8549dc924d93fe6790bd01ba149c3
variables_constantes_tipo_de_datos_26_03_2021 <81273279+AndresGelvezS@users.noreply.github.com>
2021-03-29 20:03:00 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+
+
+
+// Codigo en funcion
+function Saludar(p1){
+    console.log(`Hola ${p1}`);
+    return null;
+}
+Saludar("Fabian");
+Saludar("Andres");
+Saludar("Miguel Angel");
+Saludar("Marly");
+Saludar("Orlando");
+
+
+
+
+// Codigo ens ecuencia
+let saludar;
+saludar = "Andres";
+console.warn(`Hola ${saludar}`);
+saludar = "Miguel Angel";
+console.warn(`Hola ${saludar}`);
+saludar = "Marly";
+console.warn(`Hola ${saludar}`);
+saludar = "Orlando";
+console.warn(`Hola ${saludar}`);
+saludar = "Fabian";
+console.warn(`Hola ${saludar}`);
+
+
+
+
+
+
+
+
+
+
+// debugger;
+// let nombre;
+// let edad;
+// nombre = prompt("Ingrese su nombre");
+// edad = parseInt(prompt("Ingrese su edad"));
+// console.log(nombre);
+// console.log(typeof(nombre));
+// console.log(edad);
+// console.log(typeof(edad));
+
+
+
+
+
+
+
